4e31029db6b8191eb012a2cfcbbdddd8675ee3c9
sjhouston23 <houston.stephen23@gmail.com>
2025-04-03 16:39:17 -0500

    Working as a library

--- axio_common/database/db_core.py
+++ axio_common/database/db_core.py
@@ -14,11 +14,11 @@ FIREBASE_DB_URL = os.getenv("FIREBASE_DB_URL")
 FIREBASE_BUCKET = os.getenv("FIREBASE_BUCKET")
 SERVER_NAME = os.getenv("SERVER_NAME")
 
-# logger.info(f"Using database URL: {DATABASE_URL}")
-# logger.info(f"Using Firebase credentials: {FIREBASE_CREDENTIALS}")
-# logger.info(f"Using Firebase database URL: {FIREBASE_DB_URL}")
-# logger.info(f"Using Firebase bucket: {FIREBASE_BUCKET}")
-# logger.info(f"Using server name: {SERVER_NAME}")
+logger.info(f"Using database URL: {DATABASE_URL}")
+logger.info(f"Using Firebase credentials: {FIREBASE_CREDENTIALS}")
+logger.info(f"Using Firebase database URL: {FIREBASE_DB_URL}")
+logger.info(f"Using Firebase bucket: {FIREBASE_BUCKET}")
+logger.info(f"Using server name: {SERVER_NAME}")
 
 # Initialize Firebase
 cred = credentials.Certificate(FIREBASE_CREDENTIALS)
